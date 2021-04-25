972bdaf4517c6a9720b2c31cd25728250d779bc4
MatthewTGM <matthewtgm120@gmail.com>
2021-04-25 09:25:23 +0200

    More multithreading utilities

--- src/main/java/ga/matthewtgm/lib/util/Multithreading.java
+++ src/main/java/ga/matthewtgm/lib/util/Multithreading.java
@@ -1,14 +1,13 @@
 package ga.matthewtgm.lib.util;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Multithreading {
 
     private static final AtomicInteger threadCount = new AtomicInteger(0);
-    private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(50, 50, 0l, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), (r) -> new Thread(r, String.format("Thread %s", threadCount.incrementAndGet())));
+    private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(50, 50, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), (r) -> new Thread(r, String.format("Thread %s", threadCount.incrementAndGet())));
+    private static final ScheduledExecutorService runnableExecutor = Executors.newScheduledThreadPool(10, r -> new Thread("TGMLib Thread " + threadCount.incrementAndGet()));
 
     /**
      * @param runnable The code to run asynchronously.
@@ -16,6 +15,35 @@ public class Multithreading {
      */
     public static void runAsync(Runnable runnable) {
         executor.execute(runnable);
+    }
+
+    /**
+     * @param runnable The runnable code to be ran.
+     * @param delay    The delay before running.
+     * @param timeUnit The time unit of the delay.
+     * @author MatthewTGM
+     */
+    public static ScheduledFuture<?> schedule(Runnable runnable, long delay, TimeUnit timeUnit) {
+        return runnableExecutor.schedule(runnable, delay, timeUnit);
+    }
+
+    /**
+     * @param runnable   The runnable code to be ran.
+     * @param startDelay The initial delay before running the first time.
+     * @param delay      The delay before running.
+     * @param timeUnit   The time unit of the delay.
+     * @author MatthewTGM
+     */
+    public static ScheduledFuture<?> schedule(Runnable runnable, long startDelay, long delay, TimeUnit timeUnit) {
+        return runnableExecutor.scheduleAtFixedRate(runnable, startDelay, delay, timeUnit);
+    }
+
+    /**
+     * @param runnable The code to submit.
+     * @author MatthewTGM
+     */
+    public static Future<?> submit(Runnable runnable) {
+        return executor.submit(runnable);
     }
 
 }
